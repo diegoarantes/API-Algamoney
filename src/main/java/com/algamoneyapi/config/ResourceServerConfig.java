9d24394eaecc401b7a5b6dc53d4959242694b317
Diego Arantes <dyegoh16@gmail.com>
2018-05-06 21:14:20 -0300

    Terminado aula 6.3 - Oauth2, Password Flow

---
+++ src/main/java/com/algamoneyapi/config/ResourceServerConfig.java
@@ -0,0 +1,35 @@
+package com.algamoneyapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration // opcional
+@EnableWebSecurity
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    /*
+     * Não necessita estar autenticado para executar requisição em '/teste',
+     * mas precisa para executar qualquer outra requisição.
+     * API REST não criará sessão no servidor, ou seja, não manterá estado de nada.
+     * Cross site desabilitado -> javascript injection
+     */
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/categorias").permitAll()
+			.anyRequest().authenticated()
+			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Não mantem estado de nada no Servidor
+			.and().csrf().disable();
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
+	}
+}
--- src/main/java/com/algamoneyapi/config/SecurityConfig.java
+++ src/main/java/com/algamoneyapi/config/SecurityConfig.java
@@ -1,30 +1,34 @@
 package com.algamoneyapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configuration // opcional
+/*
+* A anotação ResourceServerConfigurerAdapter gera um WebSecurityConfigurerAdapter.
+* A Order do WebSecurityConfigurerAdapter gerado pelo ResourceServerConfigurerAdapter é 3.
+* A Order deste WebSecurityConfigurerAdapter deve ser > 3, para que o configure(HttpSecurity http) do
+* ResourceServerConfig tenha precedência sobre o configure(HttpSecurity http) do SecurityConfig.
+*/
+@Configuration
 @EnableWebSecurity
+@Order(4)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("admin").password("{noop}admin").roles("ROLE");
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
 		// https://stackoverflow.com/questions/46999940/spring-boot-passwordencoder-error
 	}
 
+	@Bean
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/categorias").permitAll()
-			.anyRequest().authenticated()
-			.and().httpBasic()
-			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // Não mantem estado de nada no Servidor
-			.and().csrf().disable();
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 }
