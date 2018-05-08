abbcdc944dce0e35c784b0e9046b4d6e504b6a41
D.Arantes <D.Arantes@TI-002.INCOPOSTES.COM>
2018-05-08 10:46:06 -0300

    Terminado aula 7.7 - Desafio: Pesquisa de pessoa

--- src/main/java/com/algamoneyapi/repository/PessoaRepository.java
+++ src/main/java/com/algamoneyapi/repository/PessoaRepository.java
@@ -1,9 +1,12 @@
 package com.algamoneyapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algamoneyapi.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 
+	public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 }
--- src/main/java/com/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/algamoneyapi/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.algamoneyapi.resource;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,12 +40,6 @@ public class PessoaResource {
 
 	@Autowired
 	private ApplicationEventPublisher publisher;
-
-	@GetMapping
-	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
-	private List<Pessoa> listar() {
-		return pessoaRepository.findAll();
-	}
 
 	@PostMapping
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
@@ -83,5 +79,11 @@ public class PessoaResource {
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
 	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
 		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	}
+
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
+	public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "%") String nome, Pageable pageable) {
+		return pessoaRepository.findByNomeContaining(nome, pageable);
 	}
 }
